d7ba6170b64eeacb01ec46d5a452dd08602dc78f
phentos <40075433+phentos@users.noreply.github.com>
2026-05-09 16:19:11 -0700

    designing the mob scene

---
+++ mob.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+@export var min_speed = 10
+@export var max_speed = 18
+
+func _physics_process(delta):
+	move_and_slide()
+	
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position)
+	rotate_y(randf_range(-PI/4, PI/4))
+	
+	var speed = randi_range(min_speed, max_speed)
+	velocity = (Vector3.FORWARD * speed).rotated(Vector3.UP, rotation.y)
+	
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
