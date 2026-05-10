3461f03fd67c037815079ecc2a2544e49670a1f2
phentos <40075433+phentos@users.noreply.github.com>
2026-05-09 20:04:24 -0700

    about to squash

--- player.gd
+++ player.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
+@export var bounce_impulse = 30
 
 # "3d vector combining speed with direction"
 var target_velocity = Vector3.ZERO
@@ -27,7 +29,13 @@ func _physics_process(delta):
 	
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	else:
+		if Input.is_action_just_pressed("jump"):
+			target_velocity.y = jump_impulse
 	
 	velocity = target_velocity
+	
 	move_and_slide()
 	
+func process_collisions():
+	pass
